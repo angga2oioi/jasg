bc6f3df3bf0980761e331ea94b3b1ad3e8429c32
angga <angga2oioi@gmail.com>
2019-03-23 13:13:15 +0700

    multithreading

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,19 @@
 
 const lib = require("./../lib/jasg.js");
 const fs = require("fs");
+const msToHMS = ( ms ) =>{
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = seconds % 60;
+    return( hours +" Hours, "+minutes+" Minutes, "+seconds+" Seconds");
+}
+
 var argv =process.argv.slice(2);
 var params={};
 
@@ -21,9 +34,10 @@ if(params.h===true){
 	return;
 }
 if(params.url && typeof params.url ==="string"){
-    lib.start(params.url,(response)=>{
+    lib.start({url:params.url},(response)=>{
         console.log(response);
     })
     return;
 }
+
 console.log("please run jasg -h for info");
